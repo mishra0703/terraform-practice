0b39b44660de719ef7ff9d3a2e444ed43b4cb403
Prem Mishra <premmishra874478@gmail.com>
2026-09-24 20:11:02 +0530

    Added solution for Chicken-and-Egg Trap

--- remote-backends/terraform.tf
+++ remote-backends/terraform.tf
@@ -6,6 +6,12 @@ terraform {
     }
   }
 
+
+# Terraform can't initialize a backend against a bucket that isn't there
+# So first we need to comment this backend and have to create s3 bucket and dynamoDB table 
+# terraform init → terraform apply 
+# A new bucket and dynamo table will get created , then we can uncomment this backend block and Re-init the infrastructure to migrate the state from local → S3
+
 backend "s3" {
     bucket         = "remote-bucket-by-prem"
     dynamodb_table = "remote-dynamoDB-table"
